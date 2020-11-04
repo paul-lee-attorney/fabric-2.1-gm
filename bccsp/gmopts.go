6ceae2125538013b33f7f27e029e1f962d6e68fc
paul-lee-attorney <paul_lee0919@163.com>
2020-11-04 18:06:50 +0800

    changed into factory mode

--- bccsp/gmopts.go
+++ bccsp/gmopts.go
@@ -1,5 +1,5 @@
 /*
-Copyright Paul Lee 2020 All Rights Reserved.
+Copyright Paul Lee 2020.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
